649e0a782681e75b371f7bca831ef4ae91132b1f
Doeke Zanstra <doekman@icloud.com>
2020-06-17 18:40:58 +0200

    Update nextcloud.sql

--- nextcloud.sql
+++ nextcloud.sql
@@ -36,28 +36,39 @@ create view nextcloud.log$parse_errors as
 
 drop view if exists nextcloud.log$all;
 create or replace view nextcloud.log$all as
-	select id
-	,      COALESCE((doc#>>'{message,Code}')::int, -1)
-	                                       as code
-	,      COALESCE(NULLIF(doc#>>'{message,Message}',''),doc#>>'{message,CustomMessage}',NULLIF(doc#>>'{message}',''))
-	                                       as message
-	,      doc#>>'{message,CustomMessage}' as custom_message
-	,      doc#>>'{time}'                  as time
-	,      doc#>>'{user}'                  as user
-	,      (doc#>>'{level}')::int          as level
-	,      doc#>>'{app}'                   as app
-	,      doc#>>'{message,Exception}'     as exception
-	,      doc#>>'{message,File}'          as file
-	,      (doc#>>'{message,Line}')::int   as line
-	,      doc#>>'{message,Trace}'         as trace
-	,      doc#>>'{version}'               as version
-	,      doc#>>'{remoteAddr}'            as remoteAddr
-	,      doc#>>'{userAgent}'             as userAgent
-	,      doc#>>'{reqId}'                 as reqId
-	,      doc#>>'{url}'                   as url
-	,      doc#>>'{method}'                as method
-	,      replace(doc#>>'{time}','T',' ')::timestamp with time zone 
-	                                       as timestamp_time
-	from nextcloud.log
-	where not doc @> '{"log_line_status":true}'::jsonb
+	with baseline as (
+		select id
+		,      COALESCE((doc#>>'{message,Code}')::int, -1)
+		                                           as code
+		,      COALESCE(NULLIF(doc#>>'{message,Message}',''),doc#>>'{message,CustomMessage}',NULLIF(doc#>>'{message}',''))
+		                                           as message
+		,      doc#>>'{message,CustomMessage}'     as custom_message
+		,      doc#>>'{time}'                      as time_text
+		,      doc#>>'{user}'                      as user_name -- "user" needs to be double-quoted
+		,      (doc#>>'{level}')::int              as level
+		,      doc#>>'{app}'                       as app
+		,      doc#>>'{message,Exception}'         as exception
+		,      doc#>>'{message,File}'              as file
+		,      (doc#>>'{message,Line}')::int       as line
+		,      doc#>>'{message,Trace}'             as trace
+		,      doc#>>'{version}'                   as version
+		,      doc#>>'{remoteAddr}'                as remote_addr
+		,      doc#>>'{userAgent}'                 as user_agent
+		,      doc#>>'{reqId}'                     as req_id
+		,      replace(doc#>>'{url}', '%20', ' ')  as url
+		,      doc#>>'{method}'                    as method
+		from nextcloud.log
+		where not doc @> '{"log_line_status":true}'::jsonb
+	)
+	select	id, message, time_text, user_name
+	,		case when url ilike '/remote.php/dav/files/%'   then substring(url from '/remote.php/dav/files/[^/]+(/.*)')
+				when url ilike '/remote.php/webdav/%'      then substring(url from '/remote.php/webdav(/.*)')
+				when url ilike '/public.php/webdav/%'      then substring(url from '/public.php/webdav(/.*)')
+				when url ilike '/remote.php/dav/uploads/%' then substring(url from '/remote.php/dav/uploads/[^/]+(/.*)')
+				else NULL
+			end path
+	,		remote_addr, user_agent, method, url
+	,		req_id, code, level, app, custom_message, exception, file, line, trace, version
+	,		replace(time_text, 'T', ' ')::timestamp with time zone as time_timestamp
+	from baseline
 ;
